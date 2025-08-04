dac64712077e7d4bdeae2e37d10ddbddfc0e1aba
PazTorr3s <paztorres05@gmail.com>
2025-08-04 15:10:46 -0400

    update work

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,25 @@
-/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form');
+const contactMessage = document.getElementById('contact-message');
+
+emailjs.init('yMjwuXDp-T1wo2QRk');
+
+const sendEmail= (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_34zx9m7','template_peivdz4', contactForm)
+    .then(() => {
+        contactMessage.textContent = 'Mensaje enviado correctamente'
+
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        contactForm.reset()
+    })
+        .catch((error) => {
+            contactMessage.textContent = 'Error al enviar el mensaje';
+            console.error('EmailJS error:', error);
+        });
+}
+
+contactForm.addEventListener('submit', sendEmail);
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,29 @@
-/*=============== SERVICES MODAL ===============*/
+const modal = document.querySelectorAll('.services__modal')
+const modalButton = document.querySelectorAll('.services__button')
+const modalClose = document.querySelectorAll('.services__modal-close')
 
 
-/*=============== SWIPER TESTIMONIAL ===============*/
+let activeModal = (modalClick) =>{
+    modal[modalClick].classList.add('active-modal')
+}
 
+modalButton.forEach((modalButton, i) =>{
+    modalButton.addEventListener('click', () =>{
+        activeModal(i)
+    })
+})
 
-/*=============== SHOW SCROLL UP ===============*/ 
+modalButton.forEach((modalClose) => {
+    modalClose.addEventListener('click', () =>{
+        modal.forEach((modalRemove) =>{
+            modalRemove.classList.remove('active-modal')
+        })
+    })
+})
+
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
